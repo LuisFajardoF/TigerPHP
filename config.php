ed3126aa3ed1af4696efc8e65185cea0d095a5d6
LuisFajardoF <luis.fajardozap@gmail.com>
2024-05-09 11:25:27 -0600

    3.6 Archivos dedicados para configuracion

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'database' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'todos',
+        'user' => 'root',
+        'password' => 'dendritas',
+    ],
+    'error_handling' => true,
+];
