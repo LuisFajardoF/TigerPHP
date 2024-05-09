a91873371795194864b5f89b2fa8a71c62d11092
LuisFajardoF <luis.fajardozap@gmail.com>
2024-05-09 08:26:53 -0600

    2.1 Creacion de clases e instanciacion de objetos

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'functions.php';
+require 'Models/Task.php';
 
 $tasks = [
-    [
-        'title' => 'Estudiar PHP',
-        'completed' => true,
-    ],
-    [
-        'title' => 'Ir al supermercado',
-        'completed' => false,
-    ],
-    [
-        'title' => 'Proyectarse',
-        'completed' => false,
-    ]
+    new Task('Estudiar PHP', true),
+    new Task('Hacer ejercicio', false),
+    new Task('Proyectarse', false),
 ];
 
 $completedTasks = array_filter($tasks, function ($task) {
-    return $task['completed'];
+    return $task->completed;
 });
 
 $pendingTasks = array_filter($tasks, function ($task) {
-    return !$task['completed'];
+    return !$task->completed;
 });
 
 require 'index.view.php';
