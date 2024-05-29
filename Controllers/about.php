233658db992ec48ad5f60f930b51081d359c6e96
LuisFajardoF <luis.fajardozap@gmail.com>
2024-05-29 15:10:32 -0600

    4.10 Modelo y tabla de usuarios

--- Controllers/about.php
+++ Controllers/about.php
@@ -1,12 +1,6 @@
 <?php
 
-// session_start();
-// dd($_SESSION['foo']);
-// dd(session_id());
-// session_destroy();
-
-// $2y$10$pMf9EAiEmHZm8P/IctheUe0/o5odn5QTpMebHvVUVxE9T3yVod6sa
-dd(password_verify('secret', '$2y$10$pMf9EAiEmHZm8P/IctheUe0/o5odn5QTpMebHvVUVxE9T3yVod6sa'));
-
+$users = User::findBy(['email' => 'luis.fajardo@todos.com']);
+dd($users[0]->password);
 
 require 'Views/about.view.php';
